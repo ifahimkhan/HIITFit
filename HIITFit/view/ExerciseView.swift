ba58da908e2b235338abe4f7951859335fe555a8
Fahim Khan <fahim.khan@tagitglobal.com>
2024-01-28 11:21:44 +0400

    Initial Commit

---
+++ HIITFit/view/ExerciseView.swift
@@ -0,0 +1,26 @@
+
+import SwiftUI
+
+struct ExerciseView: View {
+    let videosNames = ["squat","step-up","burpee","sun-salute"]
+    let exerciseNames = ["Squat","Step Up","Burpee","Sun Salute"]
+    let index:Int
+    var body: some View {
+        VStack{
+            HeaderView(exerciseName: exerciseNames[index])
+            Text("Video Player")
+            Text("Timer")
+            Text("Start/Done button")
+            Text("Rating")
+            Text("History button")
+
+        }
+    }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseView(index: 0)
+    }
+}
+
---
+++ HIITFit/view/HeaderView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct HeaderView: View {
+    let exerciseName: String
+    var body: some View {
+        VStack {
+            Text(exerciseName)
+                .font(.largeTitle)
+            HStack {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+                Image(systemName: "4.circle")
+            }.font(.title2)
+        }
+    }
+}
+
