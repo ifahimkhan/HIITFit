6c1cb4f9b1bb664c38fc2661fc826cc8e606ff29
ifahimkhan <khan.fahim95@hotmail.com>
2024-03-09 23:10:56 +0400

    If applied this commit will add exercise view a diff look. and header view

--- HIITFit/view/ExerciseView.swift
+++ HIITFit/view/ExerciseView.swift
@@ -11,7 +11,7 @@ struct ExerciseView: View {
     let interval: TimeInterval = 30
     @Binding var selectedTab: Int
     
-//    @AppStorage("rating") private var rating = 0
+    //    @AppStorage("rating") private var rating = 0
     @State private var showHistory = false
     @State private var showSuccess = false
     @State private var timerDone = false
@@ -36,58 +36,76 @@ struct ExerciseView: View {
         }
     }
     var startButton: some View{
-        Button("Start Exercise"){
+        RaisedButton(buttonText: "Start Exercise"){
             showTimer.toggle()
         }
         
     }
-    
+
+    var historyButton: some View{
+        Button(action: {
+            showHistory = true
+        }, label: {
+            Text("History")
+                .fontWeight(.bold)
+                .padding([.leading,.trailing],5)
+        })
+        .padding(.bottom,10)
+        .buttonStyle(EmbossedButtonStyle())
+    }
     var body: some View {
         
         GeometryReader { geometry in
             VStack{
                 HeaderView(selectedTab: $selectedTab,titleText: exercise.exerciseName)
                     .padding(.bottom)
-                
-                VideoPlayerView(videoName:exercise.videoName).frame(height: geometry.size.height * 0.45)
-                
-                
-                
-                //                Text(Date().addingTimeInterval(interval),style: .timer)
-                //                    .font(.system(size: geometry.size.height * 0.07))
-                HStack (spacing:150){
-                    startButton
-                    doneButton
-                        .disabled(!timerDone)
-                        .sheet(isPresented:$showSuccess){
-                            SuccessView(selectedTab: $selectedTab)
-                                .presentationDetents([.medium,.large])
-                        }
-                    
-                }
-                .font(.title3)
-                .padding()
-                if showTimer{
-                    TimerView(timerDone: $timerDone, size: geometry.size.height * 0.07)
-                }
                 Spacer()
-                RatingView(exerciseIndex: index).padding()
-                Button("History"){
-                    showHistory.toggle()
-                }.sheet(isPresented: $showHistory){
-                    HistoryView(showHistory: $showHistory)
-                }
-                .padding(.bottom)
-                
+                ContainerView{
+                    VStack{
+
+
+                        VideoPlayerView(videoName:exercise.videoName).frame(height: geometry.size.height * 0.45)
+
+
+
+                        //                Text(Date().addingTimeInterval(interval),style: .timer)
+                        //                    .font(.system(size: geometry.size.height * 0.07))
+                        HStack (spacing:150){
+                            startButton
+                            doneButton
+                                .disabled(!timerDone)
+                                .sheet(isPresented:$showSuccess){
+                                    SuccessView(selectedTab: $selectedTab)
+                                        .presentationDetents([.medium,.large])
+                                }
+
+                        }
+                        .font(.title3)
+                        .padding()
+                        if showTimer{
+                            TimerView(timerDone: $timerDone, size: geometry.size.height * 0.07)
+                        }
+                        Spacer()
+                        RatingView(exerciseIndex: index).padding()
+                        historyButton.sheet(isPresented: $showHistory){
+                            HistoryView(showHistory: $showHistory)
+                        }
+                        .padding(.bottom)
+                    }
+
+                }.frame(height:geometry.size.height*0.8)
             }
         }
+
     }
+
 }
+
 
 struct ExerciseView_Previews: PreviewProvider {
     static var previews: some View {
         ExerciseView(index: 3, selectedTab: .constant(1))
-            .environmentObject(HistoryStore())
+            .environmentObject(HistoryStore(preview: true))
     }
 }
 
--- HIITFit/view/HeaderView.swift
+++ HIITFit/view/HeaderView.swift
@@ -7,14 +7,21 @@ struct HeaderView: View {
     var body: some View {
         VStack {
             Text(titleText)
+                .foregroundColor(.white)
+                .fontWeight(.black)
                 .font(.largeTitle)
             HStack{
                 ForEach(Exercise.exercises.indices, id:\.self){ index in
-                    let fill = index == selectedTab ? ".fill" : ""
-                    Image(systemName: "\(index + 1).circle\(fill)")
-                        .onTapGesture {
-                            selectedTab = index
-                        }
+                    let fill = index == selectedTab
+                    ZStack{
+                        Circle().frame(width: 32,height: 32).opacity(fill ? 0.5 : 0.0)
+                            .foregroundColor(.white)
+                        Circle()
+                            .frame(width: 16,height: 16).foregroundColor(.white)
+
+                    }.onTapGesture {
+                        selectedTab = index
+                    }
                 }
             }.font(.title2)
         }
